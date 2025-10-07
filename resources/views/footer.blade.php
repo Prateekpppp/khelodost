324427e73a6498488f3c60e173f2461d1493e2c6
prateekawesom04-cloud <prateek.awesom.04@gmail.com>
2025-10-06 19:04:21 -0700

    updates

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -3,23 +3,23 @@
     <div class="row justify-content-center g-0">
       <!-- First Row: 2 Icons -->
       <div class="col-6 col-sm-2 footer-icon-wrapper">
-        <img src="/Images/upiIcon.svg" alt="Upi" class="footer-icon">
+        <img src="{{asset('images')}}/upiIcon.svg" alt="Upi" class="footer-icon">
       </div>
       <div class="col-6 col-sm-2 footer-icon-wrapper">
-        <img src="/Images/bankTransfer.svg" alt="Bank Transfer" class="footer-icon">
+        <img src="{{asset('images')}}/bankTransfer.svg" alt="Bank Transfer" class="footer-icon">
       </div>
 
       <!-- Second Row: 2 Icons -->
       <div class="col-6 col-sm-2 footer-icon-wrapper">
-        <img src="/Images/BeGambleAware.svg" alt="BeGambleAware" class="footer-icon">
+        <img src="{{asset('images')}}/BeGambleAware.svg" alt="BeGambleAware" class="footer-icon">
       </div>
       <div class="col-6 col-sm-2 footer-icon-wrapper">
-        <img src="/Images/cograE.svg" alt="Ecogra" class="footer-icon">
+        <img src="{{asset('images')}}/cograE.svg" alt="Ecogra" class="footer-icon">
       </div>
 
       <!-- Third Row: 1 Icon (centered on all screens) -->
       <div class="col-12 col-sm-2 footer-icon-wrapper d-flex justify-content-center">
-        <img src="/Images/gamblingCommission.svg" alt="Gambling Commission" class="footer-icon">
+        <img src="{{asset('images')}}/gamblingCommission.svg" alt="Gambling Commission" class="footer-icon">
       </div>
     </div>
 

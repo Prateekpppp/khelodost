03d780bd5736d1813400d12bbde02cf4f1f393b3
prateekawesom04-cloud <prateek.awesom.04@gmail.com>
2025-10-11 12:28:58 -0700

    test api

---
+++ app/Console/Commands/getApi.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+use Pusher\Pusher;
+use GuzzleHttp\Client;
+use App\Events\EventNotification;
+
+class getApi extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:get-api';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+        $sportname = $this->argument('sportname');
+        // $sportData = Cache::remember($sportname, 1, function () use($sportname) {
+            // $sportname = $this->argument('sportname');
+            $client = new Client(); 
+            $response = $client->get("https://marketsarket.qnsports.live/get".$sportname."matches2"); 
+            $body = $response->getBody(); 
+            $body = $response->getBody()->getContents(); 
+            // event(new EventNotification($body));
+            // return $body;
+        // });
+
+        $options = [
+            'cluster' => env('PUSHER_APP_CLUSTER'),
+            'useTLS' => true
+        ];
+
+        $pusher = new Pusher(
+            env('PUSHER_APP_KEY'),
+            env('PUSHER_APP_SECRET'),
+            env('PUSHER_APP_ID'),
+            $options
+        );
+
+        // $body = Storage::get('sports/'.$sportname.'.json');
+        
+        $body = json_decode($body,true);
+        $body = array_chunk($body,15);
+        $sportdataArray = [];
+
+        foreach ($body as $chunk) {
+            foreach ($chunk as $item) {
+                if($item['marketId']){
+                    if($item['inPlay']){
+                        $sportdataArray[] = $item;
+                    }
+                };
+            }
+        }
+        dd($body);
+        $body = array_slice($sportdataArray, 0, 10);
+        $body = json_encode($body);
+        Storage::put('sports/'.$sportname.'.json', $body);
+
+        $response = $pusher->trigger('sportsupdate', 'sportsupdate-event', ['data' => $body,'sport'=>$sportname]);
+            
+
+            // return json_decode($response->getBody(), true);
+            
+            // return User::where('active', 1)->get();
+    }
+}

59908b6a32fa2ff8168de0dd4bcd4ff2a6d0b23d
prateekawesom04-cloud <prateek.awesom.04@gmail.com>
2025-10-13 13:17:25 -0700

    up

--- resources/views/pages/eventPage.blade.php
+++ resources/views/pages/eventPage.blade.php
@@ -34,6 +34,85 @@
                 </div>
             </div>
 
+            <!-- Match Odds -->
+            <table class="table text-center mb-0 align-middle odds-table my-4 border-t-2 border-gray-300" style="display: none;">
+                <thead class="table-light">
+                    <tr>
+                        <th style="">Match Odds</th>
+                        <th style="">Back</th>
+                        <th style="">Lay</th>
+                    </tr>
+                </thead>
+                <tbody class="match_odds">
+                    <tr class="m_row0">
+                        <td class="text-start px-3">
+                            <div class="match-layout">
+                                <div class="right-side">
+                                    <div class="match_nat0">India</div>
+                                </div>
+                            </div>
+                        </td>
+
+                        <td>
+                            <a class="odd-btn back3">--</a>
+                            <a class="odd-btn back2">--</a>
+                            <a class="odd-btn back1">--</a>
+                        </td>
+                        
+                        <td>
+                            <a class="odd-btn lay lay1">--</a>
+                            <a class="odd-btn lay lay2">--</a>
+                            <a class="odd-btn lay lay3">--</a>
+                        </td>
+
+                    </tr>
+                    <tr class="m_row1">
+                        <td class="text-start px-3">
+                            <div class="match-layout">
+                                <div class="right-side">
+                                    <div class="match_nat1">India</div>
+                                </div>
+                            </div>
+                        </td>
+
+                        <td>
+                            <a class="odd-btn back3">--</a>
+                            <a class="odd-btn back2">--</a>
+                            <a class="odd-btn back1">--</a>
+                        </td>
+                        
+                        <td>
+                            <a class="odd-btn lay lay1">--</a>
+                            <a class="odd-btn lay lay2">--</a>
+                            <a class="odd-btn lay lay3">--</a>
+                        </td>
+
+                    </tr>
+                    <tr class="m_row2">
+                        <td class="text-start px-3">
+                            <div class="match-layout">
+                                <div class="right-side">
+                                    <div class="match_nat2">India</div>
+                                </div>
+                            </div>
+                        </td>
+
+                        <td>
+                            <a class="odd-btn back3">--</a>
+                            <a class="odd-btn back2">--</a>
+                            <a class="odd-btn back1">--</a>
+                        </td>
+                        
+                        <td>
+                            <a class="odd-btn lay lay1">--</a>
+                            <a class="odd-btn lay lay2">--</a>
+                            <a class="odd-btn lay lay3">--</a>
+                        </td>
+
+                    </tr>
+                </tbody>
+            </table>
+            
             <!-- Bookmaker -->
             <table class="table text-center mb-0 align-middle odds-table my-4 border-t-2 border-gray-300" style="display: none;">
                 <thead class="table-light">
@@ -113,84 +192,6 @@
                 </tbody>
             </table>
 
-            <!-- Match Odds -->
-            <table class="table text-center mb-0 align-middle odds-table my-4 border-t-2 border-gray-300" style="display: none;">
-                <thead class="table-light">
-                    <tr>
-                        <th style="">Match Odds</th>
-                        <th style="">Back</th>
-                        <th style="">Lay</th>
-                    </tr>
-                </thead>
-                <tbody class="match_odds">
-                    <tr class="m_row0">
-                        <td class="text-start px-3">
-                            <div class="match-layout">
-                                <div class="right-side">
-                                    <div class="match_nat0">India</div>
-                                </div>
-                            </div>
-                        </td>
-
-                        <td>
-                            <a class="odd-btn back3">--</a>
-                            <a class="odd-btn back2">--</a>
-                            <a class="odd-btn back1">--</a>
-                        </td>
-                        
-                        <td>
-                            <a class="odd-btn lay lay1">--</a>
-                            <a class="odd-btn lay lay2">--</a>
-                            <a class="odd-btn lay lay3">--</a>
-                        </td>
-
-                    </tr>
-                    <tr class="m_row1">
-                        <td class="text-start px-3">
-                            <div class="match-layout">
-                                <div class="right-side">
-                                    <div class="match_nat1">India</div>
-                                </div>
-                            </div>
-                        </td>
-
-                        <td>
-                            <a class="odd-btn back3">--</a>
-                            <a class="odd-btn back2">--</a>
-                            <a class="odd-btn back1">--</a>
-                        </td>
-                        
-                        <td>
-                            <a class="odd-btn lay lay1">--</a>
-                            <a class="odd-btn lay lay2">--</a>
-                            <a class="odd-btn lay lay3">--</a>
-                        </td>
-
-                    </tr>
-                    <tr class="m_row2">
-                        <td class="text-start px-3">
-                            <div class="match-layout">
-                                <div class="right-side">
-                                    <div class="match_nat2">India</div>
-                                </div>
-                            </div>
-                        </td>
-
-                        <td>
-                            <a class="odd-btn back3">--</a>
-                            <a class="odd-btn back2">--</a>
-                            <a class="odd-btn back1">--</a>
-                        </td>
-                        
-                        <td>
-                            <a class="odd-btn lay lay1">--</a>
-                            <a class="odd-btn lay lay2">--</a>
-                            <a class="odd-btn lay lay3">--</a>
-                        </td>
-
-                    </tr>
-                </tbody>
-            </table>
 
             
             <!-- Line Market -->

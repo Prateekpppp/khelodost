e9db6ce7826a04a46faba521a875db3382789334
prateekawesom04-cloud <prateek.awesom.04@gmail.com>
2025-10-13 16:05:45 -0700

    up

--- app/Console/Commands/getUpcomingEventData.php
+++ app/Console/Commands/getUpcomingEventData.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Storage;
 
 class getUpcomingEventData extends Command
 {

d206c341f5c5243912092ab2bc02297d4ef54d17
prateekawesom04-cloud <prateek.awesom.04@gmail.com>
2025-10-13 17:59:57 -0700

    updates

--- resources/views/pages/eventPage.blade.php
+++ resources/views/pages/eventPage.blade.php
@@ -192,6 +192,20 @@
                 </tbody>
             </table>
 
+            <!-- Normal-->
+            <table class="table text-center mb-0 align-middle odds-table my-4 border-t-2 border-gray-300" style="display: none;">
+                <thead class="table-light">
+                    <tr>
+                        <th style="">Normal</th>
+                        <th style="">Back</th>
+                        <th style="">Lay</th>
+                    </tr>
+                </thead>
+                <tbody class="updateNormal">
+                    
+                </tbody>
+            </table>
+
             <!-- Tied Match -->
             <table class="table text-center mb-0 align-middle odds-table my-4 border-t-2 border-gray-300" style="display: none;">
                 <thead class="table-light">
